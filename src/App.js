3f9b8bfce5efa640850629de730c4955a741aa31
Auskin26 <75899129+Auskin26@users.noreply.github.com>
2021-07-01 20:43:21 +0530

    added about page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Project from "./pages/Project";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/projects" component={Project} />
         <Route exact path="/events" component={Events} />
+        <Route exact path="/About" component={About} />
       </Switch>
     </Router>
   );
---
+++ src/pages/About.jsx
@@ -0,0 +1,24 @@
+import { Card } from "react-bootstrap";
+
+function About() {
+  return (
+    <div className=" container p-5 bg-dark text-white">
+      <h1>About us</h1>
+      <h4>What is PATTARAI</h4>
+      <p>
+        Pattarai is a group of aspiring engineers led by experienced engineers
+        who meet together to share ideas and work on new projects. We research,
+        teach, learn, and construct innovative technology. Pattarai is a project
+        club that provides students with the modern technical skills they need
+        to succeed in the workplace. Students are encouraged to learn by
+        completing real-world projects. We've been working on turning ideas into
+        reality for the past year and a half. More than 40 students participated
+        in the project, which resulted in the creation of 16 new initiatives and
+        the holding of nine events. We desire to bring a change being the
+        change!
+      </p>
+    </div>
+  );
+}
+
+export default About;
